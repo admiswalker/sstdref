eb0cf7b95d297d54b800e71033b009c46788709d
admiswalker <admiswalker@gmail.com>
2020-12-28 17:02:50 +0900

    rm usages

--- usages/src/slice_cp03.cpp
+++
@@ -1,9 +0,0 @@
-#include <sstd/sstd.hpp>
-
-int main(){
-    std::vector<int> v        = {1,2,3,4,5};
-    std::vector<int> v_sliced = v && sstd::slice(sstd::begin(),sstd::end());
-    
-    sstd::printn(v);
-    sstd::printn(v_sliced);
-}
--- usages/src/slice_mv03.cpp
+++
@@ -1,9 +0,0 @@
-#include <sstd/sstd.hpp>
-
-int main(){
-    std::vector<std::string> v        = {"1","2","3","4","5"};
-    std::vector<std::string> v_sliced = v && sstd::slice_mv(sstd::begin(),sstd::end());
-    
-    sstd::printn(v);
-    sstd::printn(v_sliced);
-}
